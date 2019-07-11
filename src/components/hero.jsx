d94cf21dc69846bfb37f0bb2f1c24ba34b20934b
Saile Marinho saile-ribeiro@hotmail.com <saile-ribeiro@hotmail.com>
2019-07-11 12:11:48 -0300

    Add buttons

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,8 +5,10 @@ class Hero extends Component {
     return (
       <div className="hero">
         <div className="container">
-          <h1>Texto</h1>
+          <h1>Saile Marinho</h1>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt quo ipsam soluta voluptatum ad non aliquid ducimus laborum vel. Soluta debitis maiores id ducimus corrupti officia, ratione earum, illo molestias. </p>
+          <div className="btn">About</div>
+          <div className="btn">Contact</div>
         </div>
       </div>
     );
